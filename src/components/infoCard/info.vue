64eaac2d0b04a9057f4945cad6e3b750f4e6dcf5
LoopMirror <plasticfish@foxmail.com>
2024-03-16 20:45:20 +0800

    update

--- src/components/infoCard/info.vue
+++ src/components/infoCard/info.vue
@@ -1,11 +1,32 @@
 <script setup>
-
+const props = defineProps(['info'])
+let card = props.info
 </script>
- 
+
 <template>
- 1231
+  <q-card class="my-card" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs">
+        <div class="text-overline">{{ card.title }}</div>
+        <div class="text-h6 q-mt-sm q-mb-xs">{{ card.content }}:{{ card.data }}%</div>
+        <div class="text-caption text-grey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </div>
+      </q-card-section>
+
+
+    </q-card-section>
+
+    
+
+
+  </q-card>
 </template>
- 
+
 <style lang='scss' scoped>
- 
+.my-card {
+
+  max-width: 300px
+}
 </style>
--- src/views/index/template/home/home.vue
+++ src/views/index/template/home/home.vue
@@ -2,14 +2,19 @@
 import { UseDarkState } from '@/store/darkState.js';
 import infoCard from '@/components/infoCard';
 const darkStore = UseDarkState()
+let info = {
+  title: 'CPU',
+  content: '已使用率',
+  data: 90
+}
 </script>
 
 <template>
   <div class="full-width row gt-sm">
     <div class="col-md-6 col-xs-12 row q-py-sm q-pr-sm">
       <div class="col-12 column blocks">
-        <div class="bg-red col">
-          <infoCard />
+        <div class="bg-red row justify-center col-gutter-sm">
+          <infoCard v-for="item in 4" class="col-6" :info="info"/>
         </div>
         <div class="bg-blue col-8">
 
