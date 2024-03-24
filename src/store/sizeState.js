53fecf31295149e61a9f04c77f67d3a0e851db48
LoopMirror <plasticfish@foxmail.com>
2024-03-24 17:09:16 +0800

    update

--- src/store/sizeState.js
+++ src/store/sizeState.js
@@ -4,9 +4,11 @@ import { ref } from 'vue'
 export const UseSizeState = defineStore('size', () => {
   let height = ref()
   let width = ref()
+  let zoom = ref()
   function sizeGet(h, w) {
     height.value = h
     width.value = w
   }
+  
   return { height, width, sizeGet }
 })
